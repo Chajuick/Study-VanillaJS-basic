e5bbc2765acdcf7f0d95209af55e34b3d7f47a79
Chajuick <ehsrktm10@gmail.com>
2024-07-01 21:17:10 +0900

    [수강] 5일차

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+// listen 하고 싶은 event는 html element mdn을 검색
+
 const title = document.getElementById('title');
 
-title.innerText = "잡았다!";
+function handleTitleOver() {
+    const currentColor = title.style.color;
+    let newColor;
+
+    if (currentColor === "blue") {
+        newColor = "green";
+    } else {
+        newColor = "blue";
+    };
+    title.style.color = newColor;
+};
+
+title.addEventListener('mouseover', handleTitleOver);
 
 const detail = document.querySelectorAll("p.detail");
 
